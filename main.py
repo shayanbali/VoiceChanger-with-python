00117cc6b88ff3259cbd080c12cdfd1991fdf2f3
shayan <shayanbali@gmail.com>
2023-03-06 16:10:05 +0330

    Specify the structure of the program

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from pathlib import Path
+import soundfile as sf
 import pitch
 import librosa
 
-
+# Load our voice wih librosa
 voice, sr = librosa.load("example.wav", sr=None, mono=False)
 if voice.ndim > 1:
     voice = voice[0, :]
 
+# a function that change the pitch of sound
+changed_pitch_voice = pitchChanger(voice, sr)
 
+
+# write final output 
+filepath = Path("example.wav")
+output_path = filepath.parent / (filepath.stem + "pitch_changed" + filepath.suffix)
+sf.write(str(output_path), changed_pitch_voice, sr)
 
 
