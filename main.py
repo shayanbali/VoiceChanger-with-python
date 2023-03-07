55a3c0c7bd24cb6febd990092e502d4511a01b74
shayan <shayanbali@gmail.com>
2023-03-07 12:11:57 +0330

    Update genderRecognition function

--- main.py
+++ main.py
@@ -6,28 +6,6 @@ import pitch
 import librosa
 import numpy as np
 
-# def genderRecgnition(file_path):
-#     # Load a speech file and extract its pitch contour
-#     file_path = file_path
-#     snd = parselmouth.Sound(file_path)
-#     pitch = snd.to_pitch()
-#
-#     # Calculate some basic statistics of the pitch contour
-#     mean_pitch = np.mean(pitch.selected_array['frequency'])
-#     median_pitch = np.median(pitch.selected_array['frequency'])
-#     pitch_range = np.max(pitch.selected_array['frequency']) - np.min(pitch.selected_array['frequency'])
-#
-#     # Use the statistics to predict the gender of the speaker
-#     if mean_pitch > 180 and pitch_range > 50:
-#         predicted_gender = 'female'
-#     elif mean_pitch < 180 and pitch_range > 50:
-#         predicted_gender = 'male'
-#     else:
-#         predicted_gender = 'unknown'
-#
-#     print('Predicted gender:', predicted_gender)
-#     print(mean_pitch)
-#     return predicted_gender
 from numpy import fft
 from scipy.io import wavfile
 
@@ -48,6 +26,14 @@ def findMax(address):
     return frequency[max_index]
 
 
+def genderRecognition(address):
+    if findMax(address) < 180:
+        label = "male"
+    else:
+        label = "female"
+
+    print("Predicted gender: ", label)
+    return label
 
 
 # a function that change the pitch according to gender
@@ -64,7 +50,7 @@ def pitchChanger(v, sr, filepath1):
 
 # Load a speech wih librosa
 
-my_file = "qdoz5-ijl21.wav"
+my_file = input("please enter your file name")
 voice, sr = librosa.load(my_file, sr=None, mono=False)
 if voice.ndim > 1:
     voice = voice[0, :]
